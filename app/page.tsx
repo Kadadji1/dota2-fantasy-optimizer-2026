ba9bf6f63536e91e9e330f50a9d8b6fd21fa5cd9
Kadadji1 <vladkononov99@gmail.com>
2026-08-01 14:25:16 -0400

    feat: render TI roster section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Optimizer from "../components/Optimizer";
+import Rosters from "../components/Rosters";
 
 export default function Home() {
-  return <Optimizer />;
+  return (
+    <>
+      <Optimizer />
+      <Rosters />
+    </>
+  );
 }
