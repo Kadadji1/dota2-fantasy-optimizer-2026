7d328af591dc378b8ded5b81f5aeaa919ce1af9a
Kadadji1 <vladkononov99@gmail.com>
2026-08-06 14:55:38 -0400

    Add homepage navigation tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import Optimizer from "../components/Optimizer";
 import Rosters from "../components/Rosters";
 
 export default function Home() {
   return (
     <>
+      <div className="homepage-tabs-wrap">
+        <nav className="page-tabs homepage-tabs" aria-label="Primary navigation">
+          <Link href="/" className="active">Fantasy Calculator</Link>
+          <Link href="/predictions">Predictions</Link>
+          <a href="/#rules">Guide</a>
+        </nav>
+      </div>
       <Optimizer />
       <Rosters />
     </>
