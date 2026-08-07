c9e15615dde5a6d79c12705851d2451a1efb1419
Kadadji1 <vladkononov99@gmail.com>
2026-08-07 12:13:49 -0400

    Use shared header on fantasy page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
-import Link from "next/link";
 import Script from "next/script";
 import Optimizer from "../components/Optimizer";
 import Rosters from "../components/Rosters";
+import SiteHeader from "../components/SiteHeader";
 
 export default function Home() {
   return (
     <>
-      <div className="homepage-tabs-wrap">
-        <nav className="page-tabs homepage-tabs primary-page-tabs" aria-label="Primary navigation">
-          <Link href="/" className="active">Fantasy Calculator</Link>
-          <Link href="/predictions">Predictions</Link>
-          <Link href="/guide">Guide</Link>
-        </nav>
+      <SiteHeader active="fantasy" />
+      <div className="calculator-page-body">
+        <Optimizer />
+        <Rosters />
       </div>
-      <Optimizer />
-      <Rosters />
       <Script src="/team-map-fix.js" strategy="afterInteractive" />
-      <Script src="/social-links.js" strategy="afterInteractive" />
       <Script src="/extra-languages.js" strategy="afterInteractive" />
       <Script src="/language-switch-fix.js" strategy="afterInteractive" />
       <Script src="/source-copy-fix.js" strategy="afterInteractive" />
